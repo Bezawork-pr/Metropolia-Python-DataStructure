26bd5cd6d02d09afd81f77e604ab3cc87f9018eb
Bezawork B Lindlof <107026397+Bezawork-pr@users.noreply.github.com>
2023-10-20 12:07:07 +0300

    Make a class called Restaurant

---
+++ Restaurant.py
@@ -0,0 +1,16 @@
+"""Make a class called Restaurant.
+The __init__() method for Restaurant should store two attributes:
+a restaurant_name and a cuisine_type. Make a method called describe_restaurant()
+that prints these two pieces of information, and a method called open_restaurant()
+that prints a message indicating that the restaurant is open."""
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        self.name = restaurant_name
+        self.cuisine_type = cuisine_type
+       
+
+    def describe_restaurant(self):
+        print("{} serves wonderful {}.".format(self.name, self.cuisine_type)) 
+        
+    def open_restaurant(self):
+        print("{} is open. Come on in!".format(self.name)) 
